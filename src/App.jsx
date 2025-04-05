cb5af8cdec54b80d98ddf18fc3909c7c49f56c29
Vanessa Do <xex.vanessa@gmail.com>
2025-04-05 11:24:41 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Greetings from "./components/Greetings";
+
+function App() {
+  return (
+    <div>
+      <h2>Hello from the Greeting Component!</h2>
+      <p>This is your first custom component in React.</p>
+    </div>
+  )
+}
+export default App;
