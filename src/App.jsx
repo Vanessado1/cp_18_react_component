3443f6468d2773ccf58164481d10e11c835f3d0b
Vanessa Do <xex.vanessa@gmail.com>
2025-04-05 11:28:21 -0400

    edited app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Greetings from "./components/Greetings";
 function App() {
   return (
     <div>
-      <h2>Hello from the Greeting Component!</h2>
+      <h2>Hello from the Greeting Component! </h2>
       <p>This is your first custom component in React.</p>
     </div>
   )
