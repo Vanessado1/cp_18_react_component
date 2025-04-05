be5b30de463983dc8b6429749665b9b9309ca217
Vanessa Do <xex.vanessa@gmail.com>
2025-04-05 11:27:52 -0400

    edited greeting component

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Greetings () {
     return (
         <div>
-            <h2>Hello from the Greeting Component!</h2>
+            <h2>Hello from the Greeting Component! </h2>
             <p>This is your first custom component in React</p>
         </div>
     );
